327510fdc3bf7bc8cd1d59aa1ef32bd3fa3e92e3
Gusted <williamzijl7@hotmail.com>
2021-04-24 21:55:12 +0200

    add 404 status

--- handlers/common/notfound.go
+++ handlers/common/notfound.go
@@ -3,6 +3,7 @@ package common
 import "github.com/gofiber/fiber/v2"
 
 func NotFound(c *fiber.Ctx) error {
+	c.Status(404)
 	return c.Render("err", fiber.Map{
 		"Title": "Page not found",
 		"Error": "404 - Your request wasn't found (゜ロ゜)",
